37ea3438ae25314df8236449077ecb77bca431dc
Apfirebolt <aspper20@gmail.com>
2025-03-03 23:31:27 +0530

    views, serializers and urls added for subreddit

--- api/views.py
+++ api/views.py
@@ -1,9 +1,19 @@
-from rest_framework.generics import ListCreateAPIView, ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView, RetrieveAPIView
+from rest_framework.generics import (
+    ListCreateAPIView,
+    ListAPIView,
+    CreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+    RetrieveAPIView,
+)
 from .serializers import (
     ListCustomUserSerializer,
     CustomUserSerializer,
     CustomTokenObtainPairSerializer,
-
+    SubRedditSerializer,
+    PostSerializer,
+    CommentSerializer,
+    PostVoteSerializer,
+    RulesSerializer,
 )
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -11,8 +21,9 @@ from rest_framework import filters
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 from accounts.models import CustomUser
+from subreddit.models import SubReddit, Post, Comment, Rules
 from rest_framework.response import Response
-from . pagination import CustomPagination
+from .pagination import CustomPagination
 
 
 class CreateCustomUserApiView(CreateAPIView):
@@ -21,12 +32,12 @@ class CreateCustomUserApiView(CreateAPIView):
     permission_classes = []
 
     def post(self, request, *args, **kwargs):
-        username = request.data.get('username')
-        email = request.data.get('email')
+        username = request.data.get("username")
+        email = request.data.get("email")
 
         if CustomUser.objects.filter(username=username).exists():
             return Response({"message": "Username already exists"}, status=400)
-        
+
         if CustomUser.objects.filter(email=email).exists():
             return Response({"message": "Email already exists"}, status=400)
 
@@ -43,10 +54,14 @@ class ListCustomUsersApiView(ListAPIView):
     serializer_class = ListCustomUserSerializer
     queryset = CustomUser.objects.all()
     permission_classes = [IsAuthenticated]
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
-    filterset_fields = ['username', 'email']
-    ordering_fields = ['username', 'email']
-    search_fields = ['username', 'email']
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
+    filterset_fields = ["username", "email"]
+    ordering_fields = ["username", "email"]
+    search_fields = ["username", "email"]
 
 
 class UserProfileApiView(RetrieveUpdateDestroyAPIView):
@@ -56,3 +71,90 @@ class UserProfileApiView(RetrieveUpdateDestroyAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+class SubRedditListCreateApiView(ListCreateAPIView):
+    serializer_class = SubRedditSerializer
+    queryset = SubReddit.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
+    filterset_fields = ["name", "description"]
+    ordering_fields = ["name", "description"]
+    search_fields = ["name", "description"]
+    pagination_class = CustomPagination
+
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+
+
+class SubRedditRetrieveUpdateDestroyApiView(RetrieveUpdateDestroyAPIView):
+    serializer_class = SubRedditSerializer
+    queryset = SubReddit.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_update(self, serializer):
+        serializer.save(creator=self.request.user)
+
+
+class PostListCreateApiView(ListCreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
+    filterset_fields = ["title", "content"]
+    ordering_fields = ["title", "content"]
+    search_fields = ["title", "content"]
+    pagination_class = CustomPagination
+
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+
+
+class PostRetrieveUpdateDestroyApiView(RetrieveUpdateDestroyAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_update(self, serializer):
+        serializer.save(creator=self.request.user)
+
+
+class RulesListCreateApiView(ListCreateAPIView):
+    serializer_class = RulesSerializer
+    queryset = Rules.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
+    filterset_fields = ["rule"]
+    ordering_fields = ["rule"]
+    search_fields = ["rule"]
+    pagination_class = CustomPagination
+
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+
+
+class RetrieveRulesApiView(RetrieveAPIView):
+    serializer_class = RulesSerializer
+    queryset = Rules.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
